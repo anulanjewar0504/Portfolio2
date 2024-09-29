68fdea30bf63810ac3917253a56dd9920769e4fe
Anurag Lanjewar <85513169+anulanjewar0504@users.noreply.github.com>
2024-09-29 20:18:50 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -37,12 +37,3 @@ function close() {
     popup.style.zIndex = '';  // Hide the popup by setting low z-index
     popup.style.transition = '0.7s ease-in-out';  // Animate hiding (optional)
 }
-// function animateProgressBar() {
-//     const progressBars = document.querySelectorAll('.progress-bar');
-
-//     progressBars.forEach(progressBar => {
-//         progressBar.style.animation = 'progressAnimationStrike 6s';
-//         // progressBar.style.width = '100%'
-
-//     });
-// }
